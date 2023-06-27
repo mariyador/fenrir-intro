c6d0c8caf41d985f1a264b14af67a3130ec54225
Mariya <mariya@MacBook-Air-Mariya.local>
2023-06-26 23:12:17 -0400

    <Wrote css file, some needed changes in index.js and index.html>

--- js/index.js
+++ js/index.js
@@ -14,41 +14,45 @@ const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
-    const skill = document.createElement('li');
-    skill.innerText = skills[i];
-    skillsList.appendChild(skill);
+  const skill = document.createElement('li');
+  skill.innerText = skills[i];
+  skill.classList.add('skills-item');
+  skillsList.appendChild(skill);
 }
-
 
 document.addEventListener('DOMContentLoaded', function() {
   const messageForm = document.forms.leave_message;
   messageForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
-  const usersName = messageForm.usersName.value;
-  const usersEmail = messageForm.usersEmail.value;
-  const usersMessage = messageForm.usersMessage.value;
-  
-  console.log('Name:', usersName);
-  console.log('Email:', usersEmail);
-  console.log('Message:', usersMessage);
-  
-  const messageSection = document.getElementById('messages');
-  const messageList = messageSection.querySelector('ul');
-  const newMessage = document.createElement('li');
-  newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> wrote: <span>${usersMessage}</span> `;
+    const usersName = messageForm.usersName.value;
+    const usersEmail = messageForm.usersEmail.value;
+    const usersMessage = messageForm.usersMessage.value;
 
-  const removeButton = document.createElement('button');
-  removeButton.innerText = 'remove';
-  removeButton.type = 'button';
-  removeButton.addEventListener('click', function() {
-    const entry = removeButton.parentNode;
-    entry.remove();
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.classList.add('message');
+
+    const messageContent = document.createElement('div');
+    messageContent.classList.add('message-content');
+    messageContent.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> wrote: <span>${usersMessage}</span>`;
+
+    const messageButtons = document.createElement('div');
+    messageButtons.classList.add('message-buttons');
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove-button');
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', function() {
+      newMessage.remove();
+    });
+
+    messageButtons.appendChild(removeButton);
+    newMessage.appendChild(messageContent);
+    newMessage.appendChild(messageButtons);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
   });
-
-  newMessage.appendChild(removeButton);
-  messageList.appendChild(newMessage);
-  messageForm.reset();
-  
- });
 });
